b1c3214a1389b885fb25e0ff680c152ed8b4c30b
Michouwou <mlevieux@e1r3p2.42.fr>
2016-01-19 17:19:41 +0100

    Ft_base is now done and should work(not tested)

--- ft_printf/ft_base.c
+++ ft_printf/ft_base.c
@@ -6,27 +6,49 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 14:02:17 by mlevieux          #+#    #+#             */
-/*   Updated: 2016/01/10 14:19:32 by mlevieux         ###   ########.fr       */
+/*   Updated: 2016/01/19 17:19:17 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-wchar_t *ft_base(intmax_t num, int base)
+static void ft_isneg(int *num, int *k, wchar_t *final);
+{
+	if (*num < 0)
+	{
+		*num = *num - 1;
+		*k = 1;
+		final[0] = '-';
+	}
+}
+
+static void ft_make_base(int *base)
+{
+	if (*base == 'o' || *base == 'O')
+		*base = 8;
+	if(*base == 'x' || *base == 'X')
+		*base = 16;
+	if (*base == 'b')
+		*base = 2;
+}
+
+wchar_t 	*ft_base(intmax_t num, int base)
 {
 	wchar_t		*final;
 	int			k;
 	intmax_t	i;
 	char		alpha[17];
 
-	if (num < 0)
+	if (num < 0 && base != 10)
 		ft_error(2);
 	i = 1;
 	k = 0;
 	ft_strcpy(alpha, "0123456789abcdef");
 	ft_wbzero(final = (wchar_t*)malloc(sizeof(wchar_t) * 200), 200);
+	ft_isneg(&num, &k, final);
+	ft_make_base(&base);
 	while ((i * (intmax_t)base) < num)
-			i = i * (intmax_t)base;
+		i = i * (intmax_t)base;
 	while (i > 0)
 	{
 		final[k++] = alpha[(num / i)];
