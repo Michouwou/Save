e35832e5263e9474fe5ad4daa7d6e3f91d2b99a4
michouwou <mlevieux42@gmail.com>
2016-01-25 19:48:21 +0000

    useful links

--- ft_printf/ft_call_float.c
+++ ft_printf/ft_call_float.c
@@ -22,9 +22,9 @@ void	ft_call_float(long double number, T_LIST *trail, wchar_t **print)
 {
 	wchar_t	*result;
 
-	number = ft_float_apply_mod(trail, number);
 	result = ft_conv_float(number, (trail->accuracy != -1) ?
 			trail->accuracy : 6);
+	number = ft_float_apply_mod(trail, number);
 	result = ft_set_width(result, trail);
 	result = ft_apply_flag(result, trail);
 	*print = ft_repstr(*print, trail->start_index, trail->end_index + 1,
