cc8fcdefcff3b6973be6b3d38d9985cf7ef0ca80
michouwou <mlevieux42@gmail.com>
2016-03-01 14:01:46 +0000

    New push_swap

--- Push_Swap_Remake/push_swap.h
+++ Push_Swap_Remake/push_swap.h
@@ -14,3 +14,19 @@ typedef struct  s_stack
     void    *bool;
 }               t_stack;
 
+typedef struct  s_sol
+{
+    char    **operations;
+    int     len;
+}               t_sol;
+
+t_sol   *ft_core(t_stack *alpha, t_stack *beta, char *options);
+t_sol   *ft_try_ab(t_stack *alpha, t_stack *beta, int prev_len);
+t_sol   *ft_atob(t_stack *alpha, t_stack *beta, int trans_len, int prev_len);
+t_sol   *ft_btoa(t_stack *alpha, t_stack *beta, int prev_len);
+t_sol   *ft_is_sorted(t_stack *alpha);
+char    *ft_ra(t_stack *alpha);
+char    *ft_rr(t_stack *alpha, *beta);
+char    *ft_sa(t_stack *alpha);
+char    *ft_ss(t_stack *alpha, t_stack *beta);
+char    *ft_pa(t_stack *alpha, t *beta);
