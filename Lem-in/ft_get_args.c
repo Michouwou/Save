27abf7f9e0d4c2f72734c538171329e05efb9a32
Michel LEVIEUX <mlevieux@e1r13p1.42.fr>
2016-03-11 10:38:26 +0100

    parsing begins

--- Lem-in/ft_get_args.c
+++ Lem-in/ft_get_args.c
@@ -6,11 +6,55 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/04 13:38:30 by mlevieux          #+#    #+#             */
-/*   Updated: 2016/03/05 11:20:16 by mlevieux         ###   ########.fr       */
+/*   Updated: 2016/03/11 10:38:06 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+t_ant		**ft_find_ants(char **args)
+{
+	int		tmp;
+	int		i;
+	t_ant	**ants;
+
+	i = 0;
+	if ((tmp = ft_atoi(args[0])) > 0)
+	{
+		ants = (t_ant**)malloc(sizeof(t_ant*) * (tmp + 1));
+		while (i < tmp)
+		{
+			ants[i] = ft_make_ant(i + 1);
+			i++;
+		}
+		ants[i] = NULL;
+	}
+	else
+		ants = NULL;
+	return (ants);
+}
+
+t_room		**ft_find_rooms(char **args, t_room **start, t_room **end)
+{
+	int		i;
+	t_room	**rooms;
+	char	**tmp;
+
+	i = 0;
+	while (args[i] && !ft_strchr(args[i], '-'))
+		i++;
+	rooms = (t_room**)malloc(sizeof(t_room*) * (i + 1));
+	i = 0;
+	while (args[i] && !ft_strchr(args[i], '-'))
+	{
+		tmp = ft_strsplit(args[i], ' ');
+		if (ft_strstr(tmp, "##")
+		rooms[i] = ft_make_room(tmp[0]);
+		i++;
+	}
+	rooms[i] = NULL;
+	return (rooms);
+}
 
 static void ft_increase_tab(char ***to_up, int len)
 {
@@ -22,7 +66,7 @@ static void ft_increase_tab(char ***to_up, int len)
     *to_up = (char**)malloc(sizeof(char*) * len + 6);
     while (i < len)
     {
-        (*to_up^)[i] = ft_strcpy(*to_up[i], tmp[i]);
+        (*to_up)[i] = ft_strcpy(*to_up[i], tmp[i]);
         i++;
     }
     i = 0;
