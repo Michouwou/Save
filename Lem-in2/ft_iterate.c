481c2b89017107b49303e7b10fdaf44a35b360dc
michouwou <mlevieux42@gmail.com>
2016-03-12 17:43:45 +0000

    ft_iterate is coded

---
+++ Lem-in2/ft_iterate.c
@@ -0,0 +1,31 @@
+
+#include "lem_in.h"
+
+int    ft_iterate(t_ant **ants, t_path **paths)
+{
+    int     i;
+    int     j;
+    
+    i = 0;
+    while (ants[i] != NULL && ants[i]->path != NULL)
+    {
+        if (!ants[i]->actual->is_end)
+        {
+            ants[i]->actual = ants[i]->path->next;
+            ft_print_move(ants[i]);
+        }
+        i++;
+    }
+    if (ants[i] == NULL)
+        return (0);
+    j = 0;
+    while (ants[i] && paths[j])
+    {
+        ants[i]->path = paths[j];
+        j++;
+        i++;
+    }
+    if (ants[i] == NULL)
+        return (0);
+    return (1);
+}
