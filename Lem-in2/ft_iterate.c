19e6eaa4f4225507e5648905086d2ac7f9075db7
Michel LEVIEUX <mlevieux@e1r5p3.42.fr>
2016-03-17 11:57:04 +0100

    ants are moving

--- Lem-in2/ft_iterate.c
+++ Lem-in2/ft_iterate.c
@@ -6,7 +6,7 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/14 12:24:52 by mlevieux          #+#    #+#             */
-/*   Updated: 2016/03/17 11:34:37 by mlevieux         ###   ########.fr       */
+/*   Updated: 2016/03/17 11:54:40 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int		ft_iterate(t_ant **ants, t_path **paths)
 	int	j;
 
 	i = 0;
-	printf("ants[0]->number : %d, ants[0]->actual exists? %s\n", ants[0]->number, ants[0]->actual ? "yes" : "no");
+	printf("path[0]->name : %s\n", paths[0]->room->name);
 	fflush(stdout);
 	while (ants[i] != NULL && ants[i]->path != NULL)
 	{
-		if (!ants[i]->actual->is_end)
+		if (!ants[i]->actual)
+			ants[i]->actual = ants[i]->path->room;
+		else if (!ants[i]->actual->is_end)
 		{
 			if (i)
 				ft_printf(" ");
@@ -32,7 +34,7 @@ int		ft_iterate(t_ant **ants, t_path **paths)
 		}
 		i++;
 	}
-	ft_printf("blop\n");
+	ft_printf("\n");
 	if (ants[i] == NULL)
 		return (0);
 	j = 0;
@@ -42,9 +44,9 @@ int		ft_iterate(t_ant **ants, t_path **paths)
 		j++;
 		i++;
 	}
-	printf("ants[0]->number : %d, ants[0]->actual exists? %s\n", ants[0]->number, ants[0]->actual ? "yes" : "no");
-	exit(0);
-	if (ants[i] == NULL)
-		return (0);
+	printf("i : %d, ants[0]->path exists? %s\n", i, ants[0]->path->room->name);
+	//exit(0);
+	//if (ants[i] == NULL)
+	//	return (0);
 	return (1);
 }
