5d074ab896684d279cea5630881d72e8e8ece422
Michel LEVIEUX <mlevieux@e1r12p2.42.fr>
2016-03-25 03:12:13 +0100

    Paths are okay

--- ft_ls/ft_get_path.c
+++ ft_ls/ft_get_path.c
@@ -6,7 +6,7 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/24 22:51:56 by mlevieux          #+#    #+#             */
-/*   Updated: 2016/03/25 00:25:12 by mlevieux         ###   ########.fr       */
+/*   Updated: 2016/03/25 03:11:45 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ void	ft_get_path(t_data **data, t_data *sub_data)
 {
 	char	*tmp;
 
-	tmp = ft_strnew(ft_strlen((*data)->path) + ft_strlen(sub_data->name) - 1);
+	tmp = ft_strnew(ft_strlen((*data)->path) + ft_strlen(sub_data->name));
 	ft_strcpy(tmp, (*data)->path);
-	ft_strcpy(tmp + ft_strlen(tmp), sub_data->name);
+	ft_strcat(tmp, "/");
+	ft_strcat(tmp, sub_data->name);
 	sub_data->path = tmp;
 }
