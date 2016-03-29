2c277a445bb54b97bd9b5b7dbec110553dfc27ab
michouwou <mlevieux42@gmail.com>
2016-03-29 21:57:08 +0000

    just saving

--- J_LIST/jlst_get_index.c
+++ J_LIST/jlst_get_index.c
@@ -2,5 +2,19 @@
 
 int     jlst_get_index(j_list *first_node, void *data, int (*compare_func)(void*, void*))
 {
+    int     ret;
+    j_list  *tmp;
     
+    ret = 0;
+    tmp = first_node;
+    while (tmp)
+    {
+        ret++;
+        if (!compare_func(first_node->data, data))
+            break;
+        tmp = tmp->next;
+    }
+    if (tmp == NULL)
+        return (-1);
+    return (ret);
 }
