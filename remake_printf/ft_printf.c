e6c5e683e312ca8e5639a2c8c4b4bc116f651cb4
Michel LEVIEUX <mlevieux@e1r12p2.42.fr>
2016-04-22 23:13:08 +0200

    Still working on printf, should be quite fast since there are many things I can take back from the previous version :)

--- remake_printf/ft_printf.c
+++ remake_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/22 15:34:27 by mlevieux          #+#    #+#             */
-/*   Updated: 2016/04/22 16:40:52 by mlevieux         ###   ########.fr       */
+/*   Updated: 2016/04/22 23:12:34 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,23 @@ int		ft_int_type(T_LIST *args_data, va_list *args, char **result)
 		return (ft_call_int(va_arg(*args, int), args_data, result));
 }
 
+int		ft_call_int(intmax_t number, T_LIST *trail, char **print)
+{
+	char    *result;
+	int		state_value;
+
+	state_value = 1;
+	result = ft_base(number, trail->format, &state_value);
+	if (trail->format == 'X')
+		ft_strtoupper(result);
+	result = ft_apply_flag(result, trail, &state_value);
+	result = ft_set_length(trail, result, &state_value);
+	result = ft_set_width(result, trail, &state_value);
+	*print = ft_repstr(*print, trail->start_index, trail->end_index + 1, result);
+	ft_move_index(&trail, trail->start_index - trail->end_index + ft_strlen(result) - 1);
+	return (state_value);
+}
+
 int		ft_char_type(T_LIST *args_data, va_list *args, char **result)
 {
 	if (args_data->format == 'C' || (args_data->format == 'c' &&
@@ -55,12 +72,51 @@ int		ft_char_type(T_LIST *args_data, va_list *args, char **result)
 		return (ft_call_char(va_arg(*args, char), args_data, result));
 }
 
+int		ft_call_char(wchar_t wc, T_LIST *trail, char **print)
+{
+	char    *result;
+	int		state_value;
+
+	state_value = 1;
+	result = (char*)ft_transfer_wchar(wc);
+	result = ft_set_width(result, trail, &state_value);
+	if (trail->flag)
+		state_value = 0;
+	result = ft_repstr(result, 0, 0, ft_strset(ft_strnew(trail->width - 1),
+				trail->width - 1, ' '));
+	*print = ft_repstr(*print, trail->start_index, trail->end_index + 1, result);
+	ft_move_index(&trail, trail->start_index - trail->end_index + ft_strlen(result) - 1);
+	return (state_value);
+}
+
 int		ft_double_type(T_LIST *args_data, va_list *args, char **result)
 {
 	if (!ft_strcmp(args_data->mod, "L"))
 		return (ft_call_float(va_arg(*args, long double), args_data, result));
 	else
 		return (ft_call_float(va_arg(args, double), args_data, result));
+}
+
+int    ft_call_float(long double number, T_LIST *trail, char **print)
+{
+	char    *result;
+	int		state_value;
+
+	state_value = 1;
+	result = ft_conv_float(number, (trail->accuracy != -1) ?
+			trail->accuracy : 6);
+	if (trail->accuracy == -2)
+		state_value = 0;
+	if (trail->format == 'e' || trail->format == 'E')
+		result = ft_conv_exp(number, trail);
+	if (trail->format == 'E')
+		ft_strtoupper(result);
+	result = ft_apply_flag(result, trail, &state_value);
+	result = ft_set_width(result, trail, &state_value);
+	*print = ft_repstr(*print, trail->start_index, trail->end_index + 1,
+			result);
+	ft_move_index(&trail, trail->start_index - trail->end_index + ft_strlen(result) - 1);
+	return (state_value);
 }
 
 int		ft_type_crossroad(T_LIST *args_data, va_list *args, char **result)
@@ -89,25 +145,23 @@ int		ft_printf(char const *fmt, ...)
 {
 	va_list		args;
 	int			state_value;
-	int			buffer;
 	T_LIST		*args_data;
 	char		result;
 
-	*buffer = 0;
 	va_start(args, fmt);
 	result = ft_strdup(fmt);
 	state_value = ft_check_fmt(fmt);
-	args_data = ft_get_args(fmt, &buffer);
+	args_data = ft_get_args(fmt);
 	while (args_data)
 	{
-		if (args_data->width == -1)
+		if (args_data->width == -10)
 			args_data->width = va_arg(args, int);
-		if (args_data->accuracy == -1)
+		if (args_data->accuracy == -10)
 			args_data->accuracy = va_arg(args, int);
 		state_value = state_value & ft_type_crossroad(args_data, &args, &result);
 		args_data = args_data->next;
 	}
 	ft_putstr(result);
 	ft_free_data_args(data_args);	
-	return (state_value ? *buffer : -1);
+	return (state_value ? ft_strlen(result) : -1);
 }
