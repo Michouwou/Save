bdaf56b00eacc1c07d40664382ad2641f2916ee8
Michel LEVIEUX <mlevieux@e1r13p1.42.fr>
2016-05-15 15:41:26 +0200

    Still working on the norme

--- Lem-in2/ft_build_path.c
+++ Lem-in2/ft_build_path.c
@@ -51,49 +51,55 @@ static void	erase_last_one(t_path *actual_path)
 	tmp->next = NULL;
 }
 
+static int	part_two(t_path **tmp, t_path **actual_path, t_room **room, t_path **all_paths)
+{
+	*tmp = *actual_path;
+	while (*tmp && (*tmp)->next)
+		*tmp = (*tmp)->next;
+	if (!*tmp)
+		*actual_path = ft_create_path(*room);
+	else
+		(*tmp)->next = ft_create_path(*room);
+	if ((*room)->is_end)
+	{
+		erase_last_one(*actual_path);
+		return (1);
+	}
+	if (!(*room)->is_end && no_option((*room)->links, *actual_path))
+	{
+		erase_last_one(*actual_path);
+		return (2);
+	}
+	return (-1);
+}
+
 t_path		*ft_build_path(t_room *room, t_path *actual_path, t_path **all_paths)
 {
 	t_path	*result;
 	t_path	*tmp;
 	int		i;
 
-	tmp = actual_path;
-	while (tmp && tmp->next)
-		tmp = tmp->next;
-	if (!tmp)
-		actual_path = ft_create_path(room);
-	else
-		tmp->next = ft_create_path(room);
-	if (room->is_end)
-	{
-		erase_last_one(actual_path);
-		return (ft_create_path(room));
-	}
-	if (!room->is_end && no_option(room->links, actual_path))
-	{
-		erase_last_one(actual_path);
-		return (NULL);
-	}
-	i = 0;
+	i = part_two(&tmp, &actual_path, &room, all_paths);
+	if (i != -1)
+		return (i == 1 ? ft_create_path(room) : NULL);
 	result = NULL;
 	tmp = NULL;
-	while (room->links && room->links[i] && room->links[i]->name)
+	while (room->links && room->links[++i] && room->links[i]->name)
 	{
 		if (room_does_not_appear(room->links[i], actual_path))
 			tmp = ft_build_path(room->links[i], actual_path, all_paths);
 		if (tmp && ft_path_is_free(tmp, all_paths))
 			result = ft_min_path(result, tmp);
-		i++;
 		tmp = NULL;
 	}
 	tmp = ft_create_path(room);
-	if (result == NULL)
+	/*if (result == NULL)
 	{
 		erase_last_one(actual_path);
 		return (NULL);
-	}
+	}*/
 	tmp->next = result;
-	result = tmp;
+	result = result ? tmp : NULL;
 	erase_last_one(actual_path);
 	return (result);
 }
