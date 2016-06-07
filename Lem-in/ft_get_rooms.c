5b80b544563b122ef723cd70766b9e28419363cd
Michel LEVIEUX <mlevieux@e1r9p19.42.fr>
2016-06-07 19:37:03 +0200

    save

--- Lem-in/ft_get_rooms.c
+++ Lem-in/ft_get_rooms.c
@@ -43,6 +43,8 @@ static void	num_space(char *str)
 	i = 0;
 	while (str[i])
 	{
+		if (str[i] == '-')
+			ft_block("Room names can't contain dashes, that's an error");
 		if (str[i] == ' ')
 			spaces++;
 		i++;
@@ -97,8 +99,9 @@ void		ft_get_rooms(char *line, int *nrooms, t_room ***rooms, int *flag)
 	increase_num_rooms(rooms, *nrooms);
 	num_space(line);
 	tmp = ft_strsplit(line, ' ');
-	if ((tmp[1] && (ft_strchr(tmp[1],'.') || ft_strchr(tmp[1],','))) ||
-		(tmp[1] && tmp[2] && (ft_strchr(tmp[2], '.') || ft_strchr(tmp[2], ','))) ||
+	if ((tmp[0] && tmp[1] && (ft_strchr(tmp[1], '.') ||
+		ft_strchr(tmp[1], ','))) || (tmp[0] && tmp[1] &&
+		tmp[2] && (ft_strchr(tmp[2], '.') || ft_strchr(tmp[2], ','))) ||
 		(tmp[1] && ft_is_number(tmp[1]) == 1 && !ft_atoi(tmp[1])) ||
 		(tmp[1] && tmp[2] && ft_is_number(tmp[2]) == 1 && !ft_atoi(tmp[2])))
 		ft_block("Coordinates must be representable as ints");
