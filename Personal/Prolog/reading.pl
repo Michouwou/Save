ce1ef49f1c15902b2830e108eea061d63de68254
michouwou <mlevieux42@gmail.com>
2016-07-12 14:02:51 +0000

    Prolog IO

--- Personal/Prolog/reading.pl
+++ Personal/Prolog/reading.pl
@@ -1,4 +1,12 @@
 
 read_file(X, Y) :-
         open(X, read, Z),
-        read(Z, Y).
+        read_multiple(Z, Y),
+        close(Z).
+
+read_multiple(I, [B|T]) :-
+        read(I, B),
+        B\=end_of_file ->
+        read_multiple(I, T);
+        !
+        .
