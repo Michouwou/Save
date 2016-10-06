ef7b1719d689a2a261c3012c1a9fbbec91f574da
michouwou <mlevieux42@gmail.com>
2016-10-06 21:03:15 +0000

    Starting design for Matcha

---
+++ 42/Matcha/header.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="header.css"/>
+    </head>
+    <body>
+        <div id='container'>
+            <div id='profile_pic'>
+                <img src="Koala.jpg"></img>
+            </div>
+            <div id='quick_info'>
+                <table>
+                    <tr>
+                        <td>
+                            <p>Login</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>Score : 258</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div id="quick_access">
+                <ul>
+                    <a href="test.php">
+                        <li>
+                            <p>Déconnexion</p>
+                        </li>
+                    </a>
+                    <a href="test.php">
+                        <li>
+                            <p>Profil</p>
+                        </li>
+                    </a>
+                    <a href="test.php">
+                        <li>
+                            <p>Parcourir</p>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
