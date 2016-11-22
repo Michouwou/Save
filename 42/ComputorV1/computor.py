aff441454c7d4997a9c4125e4519f89fa3207b24
Michel LEVIEUX <mlevieux@e3r11p9.42.fr>
2016-11-22 22:38:45 +0100

    ComputorV1 parsing seems to be okay

--- 42/ComputorV1/computor.py
+++ 42/ComputorV1/computor.py
@@ -21,7 +21,7 @@ def transforme_variables(lst, var):
 	i = 0
 	taille = len(lst)
 	while i < taille:
-		if (lst[i] == var and lst[i + 1] == '^'):
+		if (lst[i] == var and i + 1 < taille and lst[i + 1] == '^'):
 			if (lst[i + 2] == 0):
 				lst[i] = 1
 			elif(lst[i + 2] == 1):
@@ -33,6 +33,8 @@ def transforme_variables(lst, var):
 			taille -= 2
 			lst.pop(i + 1)
 			lst.pop(i + 1)
+		elif (lst[i] == var):
+			lst[i] = 'X'
 		i += 1
 	return (lst)
 
@@ -54,6 +56,28 @@ def erreur_syntaxe(chaine):
 	print("\t" + chaine)
 	sys.exit(0)
 
+def concat_vars(lst):
+	i = 0
+	taille = len(lst)
+	while i < taille:
+		if (lst[i] == '*' and i >= 1 and i + 1 < taille):
+			if (lst[i - 1] == 1 or lst[i - 1] == "X" or lst[i - 1] == "Y"):
+				lst[i - 1], lst[i + 1] = lst[i + 1], lst[i - 1]
+			if (lst[i + 1] == 1):
+				lst.pop(i)
+				lst.pop(i)
+				taille -= 2
+			elif (lst[i + 1] == 'X' or lst[i + 1] == 'Y'):
+				lst.pop(i)
+				taille -= 1
+			else:
+				sec = lst.pop(i + 1)
+				lst.pop(i)
+				lst[i - 1] *= sec
+				taille -= 2
+		i += 1
+	return (lst)
+
 def polynome_valide(chaine):
 	lst = []
 	var = 0
@@ -74,9 +98,51 @@ def polynome_valide(chaine):
 		erreur_syntaxe("Votre expression ne contient pas de variable")
 	return (lst, var)
 
+def passe_signe(lst):
+	i = 0
+	taille = len(lst)
+	while i < taille:
+		if (lst[i] == '-'):
+			lst[i + 1] *= -1
+		if (lst[i] == '-' or lst[i] == '+') :
+			lst.pop(i)
+			taille -= 1
+		i += 1
+	return (lst)
+
+def reduire_polynome(left, right):
+	i = 0
+	sub = [0] * 3
+	taille1 = len(left)
+	taille2 = len(right)
+	while (i < taille1):
+		if (type(left[i]) is int and (i == taille1 - 1 or (type(left[i + 1]) is int))):
+			sub[0] += left[i]
+		if (type(left[i]) is int and i + 1 < taille1 and left[i + 1] == 'X'):
+			sub[1] += left[i]
+		if (type(left[i]) is int and i + 1 < taille1 and left[i + 1] == 'Y'):
+			sub[2] += left[i]
+		i += 1
+	i = 0
+	while (i < taille2):
+		if (type(right[i]) is int and (i == taille2 - 1 or (type(right[i + 1]) is int))):
+			sub[0] -= right[i]
+		if (type(right[i]) is int and i + 1 < taille2 and right[i + 1] == 'X'):
+			sub[1] -= right[i]
+		if (type(right[i]) is int and i + 1 < taille2 and right[i + 1] == 'Y'):
+			sub[2] -= right[i]
+		i += 1
+	return (sub[2], "Y", sub[1], "X", sub[0])
+
 polynome = input("Entrez votre polynome : ")
 lst, var = polynome_valide(polynome)
 print(lst)
 concat_nombres(lst)
 print(lst, "\n", transforme_variables(lst, var))
-print(split_equal(lst))
+print(concat_vars(lst))
+print(passe_signe(lst))
+lst1, lst2 = split_equal(lst)
+reduced = reduire_polynome(lst1, lst2)
+print("Reduced form :", str(reduced[0]) if reduced[0] >= 0 else ("- " + str(-reduced[0]))
+	, "* " + var + "^2", ("+ " + str(reduced[2])) if reduced[2] >= 0 else ("- " + str(-reduced[2])), "* " + var + "^1",
+	("+ " + str(reduced[4])) if reduced[4] >= 0 else ("- " + str(-reduced[4])), "* " + var + "^0 = 0")
