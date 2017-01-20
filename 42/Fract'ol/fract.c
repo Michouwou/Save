7a30499c5e33c205ec91b19aa41ef866eea1a2bc
Michel LEVIEUX <mlevieux@e1r8p8.42.fr>
2017-01-20 16:15:51 +0100

    Fixed mouse_events (sometimes it is really important to read the doc! )

--- 42/Fract'ol/fract.c
+++ 42/Fract'ol/fract.c
@@ -6,7 +6,7 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 21:15:46 by mlevieux          #+#    #+#             */
-/*   Updated: 2017/01/18 19:20:56 by mlevieux         ###   ########.fr       */
+/*   Updated: 2017/01/20 16:04:33 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ void	set_fract(t_core *core, char *set)
 {
 	if (!ft_strcmp(set, "julia") || !ft_strcmp(set, "mandelbrot"))
 	{
-		core->set.set = 1;
-		core->set.iteration = 20;
+		core->set.set = !ft_strcmp("julia", set) ? 2 : 1;
+		core->set.iteration = 50;
 		core->set.roof = 4.0;
 		core->set.zoom = 1.0;
 		core->set.colorset = 1;
+		core->set.juliaC = z_num(0.3, 0.1);
 	}
 	else
 		help();
@@ -28,12 +29,50 @@ void	set_fract(t_core *core, char *set)
 
 void	draw_fract(t_core core)
 {
+	if (core.set.set == 1)
+		draw_mandelbrot(core);
+	else if (core.set.set == 2)
+		draw_julia(core);
+}
+
+void	draw_mandelbrot(t_core core)
+{
 	t_complex	current;
 	t_complex	z;
 	int			i;
 	int			j;
 	int			iter;
+	double		zoom;
 
+	i = -1;
+	mlx_clear_window(core.mlx.core, core.mlx.win);
+	zoom = core.set.zoom;
+	while (++i < core.mlx.width)
+	{
+		j = -1;
+		while (++j < core.mlx.height)
+		{
+			iter = -1;
+			current = z_num(zoom * -2.1 + (zoom * 2.7 / core.mlx.width * i),
+					zoom * -1.2 + (zoom * 2.4 / core.mlx.height * j));
+			z = z_num(0.0, 0.0);
+			while (++iter < core.set.iteration && z_mod(z) < core.set.roof)
+				z = z_add(z_pow(z, 2), current);
+			if (iter == core.set.iteration)
+				mlx_pixel_put(core.mlx.core, core.mlx.win, i, j, 0xFFFFFF);
+		}
+	}
+}
+
+void	draw_julia(t_core core)
+{
+	t_complex	z;
+	int			i;
+	int			j;
+	int			iter;
+	double		zoom;
+
+	zoom = core.set.zoom;
 	i = -1;
 	mlx_clear_window(core.mlx.core, core.mlx.win);
 	while (++i < core.mlx.width)
@@ -42,14 +81,10 @@ void	draw_fract(t_core core)
 		while (++j < core.mlx.height)
 		{
 			iter = -1;
-			current = z_num(-2.1 + ((2.7 / (core.mlx.width * core.set.zoom)) * i),
-					-1.2 + ((2.4 / (core.mlx.height * core.set.zoom)) * (core.mlx.height - j)));
-			z = z_num(0.0, 0.0);
+			z = z_num(-1.25 * zoom + (zoom * 3. / core.mlx.width * i), -1.5 * zoom + (zoom * 3. / core.mlx.height * j));
 			while (++iter < core.set.iteration && z_mod(z) < core.set.roof)
-				z = z_add(z_pow(z, 2), current);
+				z = z_add(z_pow(z, 2), core.set.juliaC);
 			if (iter == core.set.iteration)
-				mlx_pixel_put(core.mlx.core, core.mlx.win, i, j, 0x000000);
-			else
 				mlx_pixel_put(core.mlx.core, core.mlx.win, i, j, 0xFFFFFF);
 		}
 	}
--- 42/Fract'ol/main.c
+++ 42/Fract'ol/main.c
@@ -6,20 +6,50 @@
 /*   By: mlevieux <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 20:29:48 by mlevieux          #+#    #+#             */
-/*   Updated: 2017/01/18 19:17:25 by mlevieux         ###   ########.fr       */
+/*   Updated: 2017/01/20 16:12:38 by mlevieux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-static int	 key_events(int keycode, t_core *core)
+static t_core	*init_core(void)
+{
+	t_core *core;
+
+	core = (t_core*)ft_memalloc(sizeof(t_core));
+	core->mlx.width = 800;
+	core->mlx.height = 650;
+	return (core);
+}
+
+static int		key_events(int keycode, t_core *core)
 {
 	printf("key = %d\n", keycode);
 	if (keycode == 53)
 		exit(0);
 	else if (keycode == 126)
-		core->set.zoom += 0.1;
+		core->set.zoom -= 0.1;
 	else if (keycode == 125)
+		core->set.zoom += 0.1;
+	else if (keycode == 67)
+		core->set.juliaC = z_add(core->set.juliaC, z_num(0.0, 0.01));
+	else if (keycode == 75)
+		core->set.juliaC = z_add(core->set.juliaC, z_num(0.0, -0.01));
+	else if (keycode == 78)
+		core->set.juliaC = z_add(core->set.juliaC, z_num(0.01, 0.0));
+	else if (keycode == 69)
+		core->set.juliaC = z_add(core->set.juliaC, z_num(-0.01, 0.0));
+	draw_fract(*core);
+	return (0);
+}
+
+static int	mouse_events(int keycode, int x, int y, t_core *core)
+{
+	(void)x;
+	(void)y;
+	if (keycode == 4)
+		core->set.zoom += 0.1;
+	else if (keycode == 5)
 		core->set.zoom -= 0.1;
 	draw_fract(*core);
 	return (0);
@@ -33,26 +63,26 @@ void		help(void)
 
 int			main(int argc, char **argv)
 {
-	t_core core;
+	t_core *core;
 
 	if (argc < 2)
 		help();
-	core.mlx.width = 800;
-	core.mlx.height = 650;
+	core = init_core();
 	while (*(++argv))
 	{
 		if (!ft_strcmp("-width", *argv))
-			core.mlx.width = atoi(*(++argv));
+			core->mlx.width = atoi(*(++argv));
 		else if (!ft_strcmp("-height", *argv))
-			core.mlx.height = atoi(*(++argv));
+			core->mlx.height = atoi(*(++argv));
 		else
-			set_fract(&core, *argv);
+			set_fract(core, *argv);
 	}
-	core.mlx.core = mlx_init();
-	core.mlx.win = mlx_new_window(core.mlx.core, core.mlx.width,
-			core.mlx.height, "Fract'ol");
-	draw_fract(core);
-	mlx_key_hook(core.mlx.win, key_events, &core);
-	mlx_loop(core.mlx.core);
+	core->mlx.core = mlx_init();
+	core->mlx.win = mlx_new_window(core->mlx.core, core->mlx.width,
+			core->mlx.height, "Fract'ol");
+	draw_fract(*core);
+	mlx_key_hook(core->mlx.win, key_events, core);
+	mlx_mouse_hook(core->mlx.win, mouse_events, core);
+	mlx_loop(core->mlx.core);
 	return (0);
 }
